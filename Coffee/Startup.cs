14a0255e1bcbaeafde394919cedb7886738fcb07
Pawel Wisniewski <pavlo4@wp.pl>
2016-06-19 21:22:20 +0200

    Add project files.

---
+++ Coffee/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coffee.Startup))]
+namespace Coffee
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
